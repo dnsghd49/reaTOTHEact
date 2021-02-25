b2e5d441beedfb01ac86db2125167d29cd34fc76
Aaron Lu <aaronnoh0409@gmail.com>
2021-02-25 01:23:30 -0500

    axios package

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,29 +1,25 @@
 import React from "react";
-import "./style.css";
 
 // Using the datalist element we can create autofill suggestions based on the props.breeds array
 function SearchForm(props) {
   return (
-    <form className="search">
-      <div className="form-group">
-        <label htmlFor="breed">Breed Name:</label>
+    <form>
+      <div className="form-group" style={{ display: 'flex' }}>
+        <label htmlFor="search"></label>
         <input
-          value={props.search}
           onChange={props.handleInputChange}
-          name="breed"
-          list="breeds"
+          value={props.value}
+          name="search"
           type="text"
           className="form-control"
-          placeholder="Type in a dog breed to begin"
-          id="breed"
+          placeholder="Search Employee"
+          id="search"
         />
-        <datalist id="breeds">
-          {props.breeds.map(breed => (
-            <option value={breed} key={breed} />
-          ))}
-        </datalist>
-        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+        <button onClick={props.handleFormSubmit} className="btn btn-primary">
           Search
+        </button>
+        <button onClick={props.refreshPage} className="btn btn-primary ml-1">
+          Reset
         </button>
       </div>
     </form>
--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  emps: function () {
+  search: function () {
     return axios.get('https://randomuser.me/api/?results=20&nat=us');
   },
 };
