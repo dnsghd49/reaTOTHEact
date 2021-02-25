8834f959510a29a01a22f3c5adbd843ca65a49a7
Aaron Lu <aaronnoh0409@gmail.com>
2021-02-25 00:11:15 -0500

    figuring somethings out

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from './pages/main';
+//import Col from './components/Col';
+//import Display from './components/Display';
+//import SearchForm from './components/SeachForm';
 import Wrapper from './components/Wrapper';
 import Navbar from './components/Navbar';
+//import API from './utils/API';
 import "./App.css";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Wrapper>
-          <Navbar />
-          <Route exact path="/" component={Main} />
-          <Route exact path="/main" component={Main} />
-        </Wrapper>
-      </div>
-    </Router>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Wrapper>
+            <Navbar />
+            <Route exact path="/" component={Main} />
+            <Route exact path="/main" component={Main} />
+          </Wrapper>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,17 +1,9 @@
-/*
 import axios from "axios";
 
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  getRandomDog: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
+  emps: function () {
+    return axios.get('https://randomuser.me/api/?results=20&nat=us');
   },
-  getDogsOfBreed: function(breed) {
-    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  },
-  getBaseBreedsList: function() {
-    return axios.get("https://dog.ceo/api/breeds/list");
-  }
 };
-*/
