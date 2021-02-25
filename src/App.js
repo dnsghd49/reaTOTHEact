d335d71da753570639dbe60020120e4e268704e5
Aaron Lu <aaronnoh0409@gmail.com>
2021-02-24 23:23:14 -0500

    html layout before connecting the data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from './pages/main';
+import Wrapper from './components/Wrapper';
+import Navbar from './components/Navbar';
+import "./App.css";
 
 function App() {
-
+  return (
+    <Router>
+      <div className="App">
+        <Wrapper>
+          <Navbar />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/main" component={Main} />
+        </Wrapper>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
